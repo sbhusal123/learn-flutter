a2b812b3486bf5755f4bedfffe51f3ede2ae4d50
sbhusal123 <suryabhusal11@gmail.com>
2026-06-18 18:50:56 +0545

    Button and colors.

--- src/lib/main.dart
+++ src/lib/main.dart
@@ -18,15 +18,29 @@ class Home extends StatelessWidget {
         backgroundColor: Colors.red[600],
       ),
       body: Center(
-        child: Text(
-          "hello ninjas", 
-          style: TextStyle(
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
-            letterSpacing:  2.0,
-            color: Colors.grey[600],
-            fontFamily: 'IndieFlower'
-          ))
+        // child: Image(image: NetworkImage('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRF98sbQeNeSMz3nbGKwwnh4XyFU-ojyeNghA&s'),)
+        // child: Image(image: AssetImage('assets/space-2.jpg')),
+        // child: Icon(Icons.airport_shuttle, color: Colors.amber, size: 50,)
+
+        // child: IconButton(onPressed: () => print("Pressed"), icon: Icon(Icons.home))
+        // child: ElevatedButton(onPressed: () => print("Pressed"), child: Text("Elevated Button"))
+        // child: TextButton(onPressed: () => print("Pressed"), child: Text("Text Button"))
+
+        // child: ElevatedButton(
+        //   onPressed: () => print("Pressed"),
+        //   child: Text("Click Me"),
+        //   style: ElevatedButton.styleFrom(
+        //     backgroundColor: Colors.red,
+        //     foregroundColor: Colors.white,
+        //   ),
+        // )
+
+        child: IconButton(
+          icon: Icon(Icons.alternate_email),
+          onPressed: () => print("Pressed"),
+          color: Colors.amber,
+        )
+
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () => print("Pressed"),
