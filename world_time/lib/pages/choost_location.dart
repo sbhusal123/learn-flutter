284e5fbabdd6a49e18c8f8cd87823a95683071da
sbhusal123 <suryabhusal11@gmail.com>
2026-06-23 00:44:04 +0545

    Added more widgets.

--- world_time/lib/pages/choost_location.dart
+++ world_time/lib/pages/choost_location.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:world_time/services/world_time.dart';
+
 class ChooseLocation extends StatefulWidget {
   const ChooseLocation({super.key});
 
@@ -13,21 +15,30 @@ class _ChooseLocationState extends State<ChooseLocation> {
   @override
   void initState() {
     super.initState();
-    print('Init state called');
   }
+
+    List<WorldTime> locations = [
+    WorldTime(url: 'Europe/London', location: 'London', flag: 'uk.png'),
+    WorldTime(url: 'Europe/Berlin', location: 'Athens', flag: 'greece.png'),
+    WorldTime(url: 'Africa/Cairo', location: 'Cairo', flag: 'egypt.png'),
+    WorldTime(url: 'Africa/Nairobi', location: 'Nairobi', flag: 'kenya.png'),
+    WorldTime(url: 'America/Chicago', location: 'Chicago', flag: 'usa.png'),
+    WorldTime(url: 'America/New_York', location: 'New York', flag: 'usa.png'),
+    WorldTime(url: 'Asia/Seoul', location: 'Seoul', flag: 'south_korea.png'),
+    WorldTime(url: 'Asia/Jakarta', location: 'Jakarta', flag: 'indonesia.png'),
+  ];
 
   // dispose() is called when the widget / state is removed from the widget tree.
   @override
   void dispose() {
     super.dispose();
-    print('Dispose called');
   }
 
 
   // Called when building widget, or everytime when state changes
   @override
   Widget build(BuildContext context) {
-    print("BUilding UI...");
+
     return Scaffold(
       backgroundColor: Colors.grey[200],
       appBar: AppBar(
@@ -37,7 +48,31 @@ class _ChooseLocationState extends State<ChooseLocation> {
         elevation: 0, // remove drop shdaow
         iconTheme: IconThemeData(color: Colors.white), // change icon color
       ),
-      body: Text('Choose Location Screen'),
+      body: ListView.builder(
+        itemCount: locations.length,
+        itemBuilder: (context, index) {
+          return Padding(
+          padding: EdgeInsets.symmetric(vertical: 1.0, horizontal: 4.0),
+          child: Card(
+              child: ListTile(
+                onTap: () async {
+                  await locations[index].getTime();
+                  Navigator.pushReplacementNamed(context, '/home', arguments: {
+                    'location': locations[index].location,
+                    'flag': locations[index].flag,
+                    'time': locations[index].time,
+                    'isDayTime': locations[index].isDayTime,
+                  });
+                },
+                leading: CircleAvatar(
+                  backgroundImage: AssetImage('assets/images/${locations[index].flag}'),
+                ),
+                title: Text(locations[index].location),
+              ),
+            )
+          );
+        },
+      ),
     );
   }
 }
