57bb0f8f262582a5a28af9f9bbb0031bd32ab84a
sbhusal123 <suryabhusal11@gmail.com>
2026-06-22 23:30:31 +0545

    Error handling.

--- world_time/lib/services/world_time.dart
+++ world_time/lib/services/world_time.dart
@@ -13,30 +13,37 @@ class WorldTime {
 
   Future<void> getTime() async{
 
-    // make http request
-    Response response = await get(Uri.parse('https://time.now/developer/api/timezone/$url'));
+    try {
+      // make http request
+      Response response = await get(Uri.parse('https://time.now/developer/api/timezone/$url'));
 
-    // decoding JSON string into Dart object ( Map or List )
-    Map data = jsonDecode(response.body);
+      // decoding JSON string into Dart object ( Map or List )
+      Map data = jsonDecode(response.body);
 
-    // get properties from api
-    String utcDatetime = data['utc_datetime'];
-    String offset = data['utc_offset'];
+      // get properties from api
+      String utcDatetime = data['utc_datetime'];
+      String offset = data['utc_offset'];
 
-    // Extract sign, hours, and minutes from offset (e.g. "+05:45" or "-05:00")
-    String sign = offset.substring(0, 1);
-    String hours = offset.substring(1, 3);
-    String minutes = offset.substring(4, 6);
+      // Extract sign, hours, and minutes from offset (e.g. "+05:45" or "-05:00")
+      String sign = offset.substring(0, 1);
+      String hours = offset.substring(1, 3);
+      String minutes = offset.substring(4, 6);
 
-    // create a datetime object
-    DateTime now = DateTime.parse(utcDatetime);
-    if (sign == '+') {
-      now = now.add(Duration(hours: int.parse(hours), minutes: int.parse(minutes)));
-    } else {
-      now = now.subtract(Duration(hours: int.parse(hours), minutes: int.parse(minutes)));
+      // create a datetime object
+      DateTime now = DateTime.parse(utcDatetime);
+      if (sign == '+') {
+        now = now.add(Duration(hours: int.parse(hours), minutes: int.parse(minutes)));
+      } else {
+        now = now.subtract(Duration(hours: int.parse(hours), minutes: int.parse(minutes)));
+      }
+
+      // set time property
+      time = now.toString();
+    } catch(e) {
+      // print error
+      print('ERROR Getting time: $e');
+
+      time = 'could not get time';
     }
-    
-    // set time property
-    time = now.toString();   
   }
 }
